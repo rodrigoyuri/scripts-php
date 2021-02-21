ecf246a341b35ee7e8242e795c331a6e8687ad75
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2021-02-21 18:44:00 -0300

    pasta com scripts e script de seculo-ano criado

---
+++ scripts/seculo-ano/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function seculoAno($ano)
+{
+  $seculo = 1;
+  $ano = substr($ano, -4);
+  $array_ano  = array_map('intval', str_split($ano));
+  $seculo = (int)sprintf( "%d%d", $array_ano[0], $array_ano[1]);
+
+  if($array_ano[2] > 0 || $array_ano[3] > 0) 
+  {
+    echo sprintf( "Seculo: %d \n", ($seculo + 1));
+    return;
+  }
+  echo sprintf( "Seculo: %d \n", $seculo);
+}
+
+seculoAno($argv[1]);
